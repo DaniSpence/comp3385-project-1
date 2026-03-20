c22e599ab8b0ef26452098cf4e8c4dde3823a8ce
Daniela Spencer <danielaspencer9@gmail.com>
2026-03-20 14:58:17 -0400

    Initial commit - Laravel starter

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function create(){
+        return view('books.create');
+    }
+
+    public function store(Request $request){
+    
+        $validatedData = $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'course_code' => 'required',
+            'price' => 'required|numeric',
+            'condition' => 'required',
+            'description' => 'required',
+            'photo' => 'required|image|mimes:jpeg,png,jpg,gif,webp',
+        ]);
+
+        $photoPath = $request->file('photo')->store('photos', 'public');
+        $filename = basename($photoPath);
+
+        Book::create([
+            'title' => $validatedData['title'],
+            'author' => $validatedData['author'],
+            'course_code' => $validatedData['course_code'],
+            'price' => $validatedData['price'],
+            'condition' => $validatedData['condition'],
+            'description' => $validatedData['description'],
+            'photo' => $filename,
+        ]);
+
+        return redirect('/books')->with('success', 'Book listing added successfully!');
+    }
+
+    public function index(){
+        $books = Book::latest()->get();
+        return view('books.index', compact('books'));
+    }
+
+    public function show(Book $book){
+        return view('books.show', compact('book'));
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+
+Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{book}', [BookController::class, 'show'])->name('books.show');
