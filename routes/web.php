9cb78c3d7a6617c9045a31fb3d518335c078e577
Daniela Spencer <danielaspencer9@gmail.com>
2026-03-20 16:06:54 -0400

    final

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-
+Route::get('/', function () {
+    return redirect('/books');
+});
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
 Route::post('/books', [BookController::class, 'store'])->name('books.store');
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
